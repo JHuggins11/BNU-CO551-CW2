7a0d8d5b09d4ddc358e2e06dbb6eec88512b2024
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-04-19 15:54:50 +0100

    Started task 1 (not finished yet).

---
+++ task1_seedstudents.php
@@ -0,0 +1,20 @@
+<?php
+    include("_includes/config.inc");
+    include("_includes/dbconnect.inc");
+    include("_includes/functions.inc");
+
+    // Insert 5 student records into the database
+    $sql = "INSERT INTO students VALUES ";
+    $sql .= "('20000001', '" . password_hash("password1", PASSWORD_DEFAULT) . "', '1980-08-25', 
+        'Terry', 'Walker', '25 Maine Way', 'High Wycombe', 'Bucks', 'UK', 'HP12 1ZZ'), ";
+    $sql .= "('20000002', '" . password_hash("password2", PASSWORD_DEFAULT) . "', '1970-05-23', 
+        'Elizabeth', 'Wayne', '7 Dry Avenue', 'Henley-on-Thames', 'Oxfordshire', 'UK', 'RG11 2FE'), ";
+    $sql .= "('20000003', '" . password_hash("password3", PASSWORD_DEFAULT) . "', '1982-03-15', 
+        'Max', 'Turner', '12 River Drive', 'Henley-on-Thames', 'Oxfordshire', 'UK', 'RG15 5ES'), ";
+    $sql .= "('20000004', '" . password_hash("password4", PASSWORD_DEFAULT) . "', '1980-08-25', 
+        'Sarah', 'Reed', '9 Hill Road', 'High Wycombe', 'Bucks', 'UK', 'HP17 3HE'), ";
+    $sql .= "('20000005', '" . password_hash("password5", PASSWORD_DEFAULT) . "', '1980-08-25', 
+        'Chloe', 'Hart', '15 Rain Street', 'Henley-on-Thames', 'Oxfordshire', 'UK', 'RG22 7WQ');";
+
+    $result = mysqli_query($conn, $sql);
+?>
