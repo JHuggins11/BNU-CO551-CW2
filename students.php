bba6820b0a4450e5d1d6b67c8229b5ab89eacc40
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-04-25 21:26:39 +0100

    Started attempt of task 3, not finished yet.

--- students.php
+++ students.php
@@ -1,4 +1,4 @@
-<!-- CO551 CW2 - Task 2 -->
+<!-- CO551 CW2 - Tasks 2-3 -->
 <!DOCTYPE html>
 <html>
     <head></head>
@@ -12,7 +12,6 @@
             if (isset($_SESSION['id'])) {
                 // Select all records from the student table
                 $sql = "SELECT studentid, password, dob, firstname, lastname, house, town, county, country, postcode FROM student";
-
                 $result = mysqli_query($conn, $sql);
 
                 // Create HTML table
@@ -20,7 +19,7 @@
                 echo "<tr><th align='left'>Student ID</th><th align='left'>Password</th><th align='left'>Date of Birth</th>"
                     . "<th align='left'>First Name</th><th align='left'>Last Name</th><th align='left'>House Address</th>"
                     . "<th align='left'>Town/City</th><th align='left'>County</th><th align='left'>Country</th>"
-                    . "<th align='left'>Postcode</th></tr>";
+                    . "<th align='left'>Postcode</th><th>Select</th></tr>";
 
                 // Display each record from the student table in the HTML table
                 while ($row = mysqli_fetch_assoc($result)) {
@@ -35,10 +34,29 @@
                     echo "<td>" . $row["county"] . "</td>";
                     echo "<td>" . $row["country"] . "</td>";
                     echo "<td>" . $row["postcode"] . "</td>";
+                    echo "<td><input type='checkbox' name='selectedrecord'></td>";
                     echo "</tr>";
                 }
 
                 echo "</table>";
+
+                // Adding multiple line breaks so the delete button is below the table.
+                for ($i=0; $i < 12; $i++) { 
+                    echo "<br>";
+                }
+
+                echo "<input type='submit' name='btndelete' value='Delete'>";
+
+                // TODO: Needs fixing as the delete button doesn't work when records are checked.
+                // Deletes the records that have checked checkboxes.
+                if (isset($_POST['selectedrecord'], $_POST['btndelete'])) {
+                    $sql = "DELETE FROM student WHERE studentid = '$_GET[studentid]'";
+                    $result = mysqli_query($conn, $sql);
+
+                    if ($result) {
+                        echo "<p>Record(s) deleted.</p>";
+                    }
+                }
             }
         ?>
     </body>
