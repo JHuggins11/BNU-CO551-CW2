56a80a729b4f969f39a7fbf7850b1251ad0dc9c0
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-05-07 21:20:03 +0100

    Updated to use the templates.

--- students.php
+++ students.php
@@ -10,43 +10,46 @@
 
             // Account login check
             if (isset($_SESSION['id'])) {
+                echo template("templates/partials/header.php");
+                echo template("templates/partials/nav.php");
+
                 // Select all records from the student table
                 $sql = "SELECT studentid, password, dob, firstname, lastname, house, town, county, country, postcode, image FROM student";
                 $result = mysqli_query($conn, $sql);
 
-                // Create HTML table
-                echo "<table align='left' border='1'>";
-                echo "<tr><th align='left'>Student ID</th><th align='left'>Password</th><th align='left'>Date of Birth</th>"
+                // Prepare page content and create HTML table
+                $data['content'] .= "<table align='left' border='1'>";
+                $data['content'] .= "<tr><th align='left'>Student ID</th><th align='left'>Password</th><th align='left'>Date of Birth</th>"
                     . "<th align='left'>First Name</th><th align='left'>Last Name</th><th align='left'>House Address</th>"
                     . "<th align='left'>Town/City</th><th align='left'>County</th><th align='left'>Country</th>"
                     . "<th align='left'>Postcode</th><th>Image</th><th>Select</th></tr>";
 
                 // Display each record from the student table in the HTML table
                 while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $row["studentid"] . "</td>";
-                    echo "<td>" . $row["password"] . "</td>";
-                    echo "<td>" . $row["dob"] . "</td>";
-                    echo "<td>" . $row["firstname"] . "</td>";
-                    echo "<td>" . $row["lastname"] . "</td>";
-                    echo "<td>" . $row["house"] . "</td>";
-                    echo "<td>" . $row["town"] . "</td>";
-                    echo "<td>" . $row["county"] . "</td>";
-                    echo "<td>" . $row["country"] . "</td>";
-                    echo "<td>" . $row["postcode"] . "</td>";
-                    echo "<td>" . $row["image"] . "</td>";
-                    echo "<td><input type='checkbox' name='selectedrecord'></td>";
-                    echo "</tr>";
+                    $data['content'] .= "<tr>";
+                    $data['content'] .= "<td>" . $row["studentid"] . "</td>";
+                    $data['content'] .= "<td>" . $row["password"] . "</td>";
+                    $data['content'] .= "<td>" . $row["dob"] . "</td>";
+                    $data['content'] .= "<td>" . $row["firstname"] . "</td>";
+                    $data['content'] .= "<td>" . $row["lastname"] . "</td>";
+                    $data['content'] .= "<td>" . $row["house"] . "</td>";
+                    $data['content'] .= "<td>" . $row["town"] . "</td>";
+                    $data['content'] .= "<td>" . $row["county"] . "</td>";
+                    $data['content'] .= "<td>" . $row["country"] . "</td>";
+                    $data['content'] .= "<td>" . $row["postcode"] . "</td>";
+                    $data['content'] .= "<td>" . $row["image"] . "</td>";
+                    $data['content'] .= "<td><input type='checkbox' name='selectedrecord'></td>";
+                    $data['content'] .= "</tr>";
                 }
 
-                echo "</table>";
+                $data['content'] .= "</table>";
 
                 // Adding multiple line breaks so the delete button is below the table.
                 for ($i=0; $i < 12; $i++) { 
-                    echo "<br>";
+                    $data['content'] .= "<br>";
                 }
 
-                echo "<input type='submit' name='btndelete' value='Delete'>";
+                $data['content'] .= "<input type='submit' name='btndelete' value='Delete'>";
 
                 // TODO: Needs fixing as the delete button doesn't work when records are checked.
                 // Deletes the records that have checked checkboxes.
@@ -55,10 +58,18 @@
                     $result = mysqli_query($conn, $sql);
 
                     if ($result) {
-                        echo "<p>Record(s) deleted.</p>";
+                        $data['content'] .= "<p>Record(s) deleted.</p>";
                     }
                 }
+
+                // Render the template
+                echo template("templates/default.php", $data);
+            } 
+            else {
+                header("Location: index.php");
             }
+
+            echo template("templates/partials/footer.php");
         ?>
     </body>
 </html>
