1a0f9faf420a825decf6d35d27fd096e64468d76
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-05-09 13:02:42 +0100

    Added Bootstrap to navbar.

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,11 +1,21 @@
-
-<div id="main-nav">
-
-   <a href="modules.php">My Modules</a> |
-   <a href="students.php">View Students</a> |
-   <a href="assignmodule.php">Assign Module</a> |
-   <a href="addstudent.php">Add New Student</a> |
-   <a href="details.php">My Details</a> |
-   <a href="logout.php">Logout</a>
-
-</div>
+<!-- Bootstrap reference: https://getbootstrap.com/docs/5.1/components/navbar/ -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <div class="container-fluid">
+      <a class="navbar-brand">BNU</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
+         aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+         <div class="navbar-nav">
+         <a class="nav-link" href="index.php">Home</a>
+         <a class="nav-link" href="modules.php">My Modules</a>
+         <a class="nav-link" href="students.php">View Students</a>
+         <a class="nav-link" href="assignmodule.php">Assign Module</a>
+         <a class="nav-link" href="addstudent.php">Add New Student</a>
+         <a class="nav-link" href="details.php">My Details</a>
+         <a class="nav-link" href="logout.php">Logout</a>
+         </div>
+      </div>
+   </div>
+</nav>
