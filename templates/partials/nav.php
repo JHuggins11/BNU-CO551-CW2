e35f75e0ce9fc20bf144b8123f847dc512eeaedd
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-05-09 11:34:13 +0100

    Updated nav links.

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -4,6 +4,7 @@
    <a href="modules.php">My Modules</a> |
    <a href="students.php">View Students</a> |
    <a href="assignmodule.php">Assign Module</a> |
+   <a href="addstudent.php">Add New Student</a> |
    <a href="details.php">My Details</a> |
    <a href="logout.php">Logout</a>
 
