f294fbf30f21ffd6e0298dc1f5302c382d8fbda8
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-05-10 14:51:53 +0100

    Applied Bootstrap styling to form.

--- assignmodule.php
+++ assignmodule.php
@@ -26,14 +26,14 @@ if (isset($_SESSION['id'])) {
      $result = mysqli_query($conn, $sql);
 
      $data['content'] .= "<form name='frmassignmodule' action='' method='post' >";
-     $data['content'] .= "Select a module to assign<br/>";
-     $data['content'] .= "<select name='selmodule' >";
+     $data['content'] .= "<label for='selmodule' class='form-label'>Select a module to assign:</label><br/>";
+     $data['content'] .= "<select class='form-select' style='width:300px;' name='selmodule' >";
      // Display the module name sin a drop down selection box
      while($row = mysqli_fetch_array($result)) {
         $data['content'] .= "<option value='$row[modulecode]'>$row[name]</option>";
      }
      $data['content'] .= "</select><br/>";
-     $data['content'] .= "<input type='submit' name='confirm' value='Save' />";
+     $data['content'] .= "<input type='submit' class='btn btn-primary' name='confirm' value='Save' />";
      $data['content'] .= "</form>";
    }
 
