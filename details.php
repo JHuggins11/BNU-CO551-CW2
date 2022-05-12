3735f80d287e9b7d28ede3b367a5a1e37f0a4291
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-05-12 20:23:49 +0100

    Added prepared statements.

--- details.php
+++ details.php
@@ -1,5 +1,5 @@
 <?php
-// CO551 CW2 - Task 6 (Bootstrap reference: https://getbootstrap.com/docs/5.1/forms/overview/)
+// CO551 CW2 - Tasks 6-7 (Bootstrap reference: https://getbootstrap.com/docs/5.1/forms/overview/)
 
 include("_includes/config.inc");
 include("_includes/dbconnect.inc");
@@ -15,8 +15,7 @@ if (isset($_SESSION['id'])) {
    // if the form has been submitted
    if (isset($_POST['submit'])) {
 
-      // TODO: prepared statement
-      // build an sql statment to update the student details
+      /* Non-prepared statement
       $sql = "update student set firstname ='" . $_POST['txtfirstname'] . "',";
       $sql .= "lastname ='" . $_POST['txtlastname']  . "',";
       $sql .= "house ='" . $_POST['txthouse']  . "',";
@@ -25,17 +24,30 @@ if (isset($_SESSION['id'])) {
       $sql .= "country ='" . $_POST['txtcountry']  . "',";
       $sql .= "postcode ='" . $_POST['txtpostcode']  . "' ";
       $sql .= "where studentid = '" . $_SESSION['id'] . "';";
-      $result = mysqli_query($conn,$sql);
+      $result = mysqli_query($conn,$sql);*/
 
+      // Build prepared SQL statement to update student's details
+      $stmt = $conn->prepare("UPDATE student SET firstname = ?, lastname = ?, house = ?, town = ?, county = ?, 
+         country = ?, postcode = ? WHERE studentid = ?;");
+      $stmt->bind_param("ssssssss", $_POST['txtfirstname'], $_POST['txtlastname'], $_POST['txthouse'], $_POST['txttown'], $_POST['txtcounty'], 
+         $_POST['txtcountry'], $_POST['txtpostcode'], $_SESSION['id']);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      
       $data['content'] = "<p>Your details have been updated</p>";
 
    }
    else {
-      // TODO: prepared statement
-      // Build a SQL statment to return the student record with the id that
-      // matches that of the session variable.
+      /* Non-prepared statement
       $sql = "select * from student where studentid='". $_SESSION['id'] . "';";
-      $result = mysqli_query($conn,$sql);
+      $result = mysqli_query($conn,$sql);*/
+
+      // Build prepared SQL statment returning student record with id matching session variable's id
+      $stmt = $conn->prepare("SELECT * FROM student WHERE studentid = ?;");
+      $stmt->bind_param("s", $_SESSION['id']);
+      $stmt->execute();
+      $result = $stmt->get_result();
+
       $row = mysqli_fetch_array($result);
 
       // using <<<EOD notation to allow building of a multi-line string
